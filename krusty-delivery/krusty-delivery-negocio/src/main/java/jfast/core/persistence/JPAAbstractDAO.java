041e3d09759fc4b5204a7334215885f837e4087e
Marcos Vinicius Coutinho da Silva <vinicius.marcs@gmail.com>
2014-06-03 15:50:42 -0300

    Classe generica para comunicacao com infra/banco de dados

--- krusty-delivery/krusty-delivery-negocio/src/main/java/jfast/core/persistence/JPAAbstractDAO.java
+++ krusty-delivery/krusty-delivery-negocio/src/main/java/jfast/core/persistence/JPAAbstractDAO.java
@@ -3,8 +3,14 @@
  */
 package jfast.core.persistence;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.FlushModeType;
 import javax.persistence.PersistenceException;
+import javax.persistence.Query;
 
 import org.apache.log4j.Logger;
 
@@ -95,7 +101,7 @@ public class JPAAbstractDAO {
 			throw new PersistenceException("Impossível atualizar objeto",
 					e.getCause());
 		}
-		
+
 		return obj;
 	}
 
@@ -115,6 +121,99 @@ public class JPAAbstractDAO {
 			logger.error(e);
 			throw new PersistenceException("Impossível excluir objeto.",
 					e.getCause());
+		}
+	}
+
+	/**
+	 * Cria uma Query baseada nos padrões JPA
+	 * 
+	 * @param jpaQuery
+	 *            - uma query no padrão JPQL
+	 * @param startRecord
+	 *            - número do registro inicial ( 0 a todos )
+	 * @param maxResults
+	 *            - número máximo de resultados ( 0 - todos )
+	 * @param named
+	 *            - usado quando jpaQuery for uma NamedQuery definida no Entity
+	 * @throws Exception
+	 *             - se a criação da Query falhar
+	 * @since 1.0
+	 */
+	public final Query createQuery(String jpaQuery, int startRecord,
+			int maxResults, boolean named) throws Exception {
+		if (jpaQuery == null) {
+			throw new PersistenceException(
+					"Impossível realizar operação de persistência, consulta inválida");
+		}
+		if (em == null || !em.isOpen()) {
+			throw new PersistenceException(
+					"Impossível realizar operação de persistência, não conectado");
+		}
+		if (logger.isDebugEnabled()) {
+			logger.debug("Using jpaQuery: " + jpaQuery + ", start: "
+					+ startRecord + ", maxResults: " + maxResults + ", named: "
+					+ named);
+		}
+		Query query = (named ? em.createNamedQuery(jpaQuery) : em
+				.createQuery(jpaQuery));
+
+		query.setFlushMode(FlushModeType.AUTO);
+		if (logger.isDebugEnabled()) {
+			logger.debug("Created query: " + query);
+		}
+		return query;
+	}
+
+	/**
+	 * Executa uma JPQL de Consulta de dados de objetos em Lista
+	 * 
+	 * @param jpaQuery
+	 *            - uma query no padrão JPQL
+	 * @param maxResults
+	 *            - número máximo de resultados ( 0 - todos )
+	 * @param parameters
+	 *            - parametros de dados
+	 * @param named
+	 *            - usado quando jpaQuery for uma NamedQuery definida no Entity
+	 * @return list - uma lista de objetos pesquisados
+	 * @throws PersistenceException
+	 *             - se a consulta da Query falhar
+	 * @since 1.0
+	 */
+	public final List select(String jpaQuery, int maxResults,
+			HashMap parameters, boolean named) throws PersistenceException {
+		List objects = null;
+		long init = System.currentTimeMillis();
+		try {
+			Query query = createQuery(jpaQuery, 0, maxResults, named);
+			fillParameters(query, parameters);
+			objects = query.getResultList();
+		} catch (Exception e) {
+			logger.error("Unable to execute query: " + jpaQuery, e);
+			throw new PersistenceException("Impossível consultar dados", e);
+		} 
+		
+		return objects;
+	}
+
+	/**
+	 * Método auxiliar para geração de parametros de JPQL
+	 * 
+	 * @param query
+	 * @param parameters
+	 */
+	private void fillParameters(Query query, HashMap<String, Object> parameters) {
+		if (parameters != null) {
+			Iterator<String> it = parameters.keySet().iterator();
+			while (it.hasNext()) {
+				String param = it.next();
+				Object value = parameters.get(param);
+				query.setParameter(param, value);
+				if (logger.isDebugEnabled()) {
+					logger.debug("Query.setParameter param: " + param
+							+ ", value: " + value);
+				}
+			}
 		}
 	}
 
